a04ea22b0b4ce9e0200ebd1fb18f674b7fcdde5c
Lisseatt <tatianallissett27@gmail.com>
2026-03-11 22:35:04 -0600

    Mi primer commit oficial

---
+++ Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace EjercicioTriangulo_19881837
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnConsulta = new System.Windows.Forms.Button();
+            this.btnAlta = new System.Windows.Forms.Button();
+            this.btnBaja = new System.Windows.Forms.Button();
+            this.btnModificacion = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtLadoA = new System.Windows.Forms.TextBox();
+            this.txtLadoB = new System.Windows.Forms.TextBox();
+            this.txtLadoC = new System.Windows.Forms.TextBox();
+            this.txtSemiperimetro = new System.Windows.Forms.TextBox();
+            this.txtArea = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lado A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Lado B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Lado C";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(249, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // btnConsulta
+            // 
+            this.btnConsulta.Location = new System.Drawing.Point(51, 279);
+            this.btnConsulta.Name = "btnConsulta";
+            this.btnConsulta.Size = new System.Drawing.Size(75, 23);
+            this.btnConsulta.TabIndex = 5;
+            this.btnConsulta.Text = "Consulta";
+            this.btnConsulta.UseVisualStyleBackColor = true;
+            this.btnConsulta.Click += new System.EventHandler(this.btnConsulta_Click);
+            // 
+            // btnAlta
+            // 
+            this.btnAlta.Location = new System.Drawing.Point(224, 278);
+            this.btnAlta.Name = "btnAlta";
+            this.btnAlta.Size = new System.Drawing.Size(75, 23);
+            this.btnAlta.TabIndex = 6;
+            this.btnAlta.Text = "Alta";
+            this.btnAlta.UseVisualStyleBackColor = true;
+            this.btnAlta.Click += new System.EventHandler(this.btnAlta_Click);
+            // 
+            // btnBaja
+            // 
+            this.btnBaja.Location = new System.Drawing.Point(390, 278);
+            this.btnBaja.Name = "btnBaja";
+            this.btnBaja.Size = new System.Drawing.Size(75, 23);
+            this.btnBaja.TabIndex = 7;
+            this.btnBaja.Text = "Baja";
+            this.btnBaja.UseVisualStyleBackColor = true;
+            this.btnBaja.Click += new System.EventHandler(this.btnBaja_Click);
+            // 
+            // btnModificacion
+            // 
+            this.btnModificacion.Location = new System.Drawing.Point(551, 278);
+            this.btnModificacion.Name = "btnModificacion";
+            this.btnModificacion.Size = new System.Drawing.Size(75, 23);
+            this.btnModificacion.TabIndex = 8;
+            this.btnModificacion.Text = "Modificacion";
+            this.btnModificacion.UseVisualStyleBackColor = true;
+            this.btnModificacion.Click += new System.EventHandler(this.btnModificacion_Click);
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(204, 340);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 9;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(472, 339);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 10;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(345, 394);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 11;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(313, 74);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(119, 55);
+            this.btnCalcular.TabIndex = 12;
+            this.btnCalcular.Text = "Calcular Semiperimetro y Area";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(136, 76);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.TabIndex = 13;
+            // 
+            // txtLadoA
+            // 
+            this.txtLadoA.Location = new System.Drawing.Point(136, 109);
+            this.txtLadoA.Name = "txtLadoA";
+            this.txtLadoA.Size = new System.Drawing.Size(100, 20);
+            this.txtLadoA.TabIndex = 14;
+            // 
+            // txtLadoB
+            // 
+            this.txtLadoB.Location = new System.Drawing.Point(136, 146);
+            this.txtLadoB.Name = "txtLadoB";
+            this.txtLadoB.Size = new System.Drawing.Size(100, 20);
+            this.txtLadoB.TabIndex = 15;
+            // 
+            // txtLadoC
+            // 
+            this.txtLadoC.Location = new System.Drawing.Point(136, 188);
+            this.txtLadoC.Name = "txtLadoC";
+            this.txtLadoC.Size = new System.Drawing.Size(100, 20);
+            this.txtLadoC.TabIndex = 16;
+            // 
+            // txtSemiperimetro
+            // 
+            this.txtSemiperimetro.Location = new System.Drawing.Point(377, 150);
+            this.txtSemiperimetro.Name = "txtSemiperimetro";
+            this.txtSemiperimetro.Size = new System.Drawing.Size(100, 20);
+            this.txtSemiperimetro.TabIndex = 17;
+            // 
+            // txtArea
+            // 
+            this.txtArea.Location = new System.Drawing.Point(377, 188);
+            this.txtArea.Name = "txtArea";
+            this.txtArea.Size = new System.Drawing.Size(100, 20);
+            this.txtArea.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(298, 153);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Semiperimetro";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(298, 194);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Area";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtArea);
+            this.Controls.Add(this.txtSemiperimetro);
+            this.Controls.Add(this.txtLadoC);
+            this.Controls.Add(this.txtLadoB);
+            this.Controls.Add(this.txtLadoA);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.btnModificacion);
+            this.Controls.Add(this.btnBaja);
+            this.Controls.Add(this.btnAlta);
+            this.Controls.Add(this.btnConsulta);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnConsulta;
+        private System.Windows.Forms.Button btnAlta;
+        private System.Windows.Forms.Button btnBaja;
+        private System.Windows.Forms.Button btnModificacion;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtLadoA;
+        private System.Windows.Forms.TextBox txtLadoB;
+        private System.Windows.Forms.TextBox txtLadoC;
+        private System.Windows.Forms.TextBox txtSemiperimetro;
+        private System.Windows.Forms.TextBox txtArea;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjercicioTriangulo_19881837
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        System.Data.OleDb.OleDbConnection conBD = new System.Data.OleDb.OleDbConnection();
+        String sAccion = "";
+
+        public void abrirConexion()
+        {
+            // Se crea el string de conexión a la base de datos
+
+            conBD.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.16.0;Data Source=C:\Users\MINEDUCYT\Documents\Triangulos.accdb;Persist Security Info=False";
+            try
+
+            {
+                // Se abre la base de datos
+
+                conBD.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("¡Error real!: " + ex.Message);
+            }
+        }
+        public void cerrarConexion()
+        {
+            // Se chequea si está la conexión abierta
+            if (conBD.State == ConnectionState.Open)
+            {
+                // Se cierra la conexión
+                conBD.Close();
+            }
+        }
+        private void desactivarAcciones()
+        {
+            btnConsulta.Enabled = false;
+            btnAlta.Enabled = false;
+            btnBaja.Enabled = false;
+            btnModificacion.Enabled = false;
+            btnAceptar.Enabled = true;
+            btnCancelar.Enabled = true;
+        }
+        private void activarAcciones()
+        {
+            btnConsulta.Enabled = true;
+            btnAlta.Enabled = true;
+            btnBaja.Enabled = true;
+            btnModificacion.Enabled = true;
+            btnAceptar.Enabled = false;
+            btnCancelar.Enabled = false;
+            // Se cancela la acción a realizar
+            sAccion = "";
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 1. Capturamos los tres lados de los TextBox
+                double a = Convert.ToDouble(txtLadoA.Text);
+                double b = Convert.ToDouble(txtLadoB.Text);
+                double c = Convert.ToDouble(txtLadoC.Text);
+
+                // 2. Calculamos primero el semiperímetro (s)
+                double s = (a + b + c) / 2;
+
+                // 3. Calculamos el área usando la fórmula de Herón
+                // Math.Sqrt es para la raíz cuadrada
+                double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+
+                // 4. Mostramos los resultados
+                txtSemiperimetro.Text = s.ToString();
+                txtArea.Text = area.ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Asegúrate de llenar los tres lados con números.");
+            }
+
+        }
+
+        private void btnConsulta_Click(object sender, EventArgs e)
+        {
+            // Se indica la acción que hay que realizar
+            sAccion = "Consulta";
+            // Se activan y desactivan los botones correspondientes
+            desactivarAcciones();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Se cierra la conexión antes de terminar
+            cerrarConexion();
+            // Se finaliza el programa
+            this.Close();
+
+        }
+
+        private void btnAlta_Click(object sender, EventArgs e)
+        {
+            // Se indica la acción que hay que realizar
+            sAccion = "Alta";
+            // Se activan y desactivan los botones correspondientes
+            desactivarAcciones();
+
+        }
+
+        private void btnBaja_Click(object sender, EventArgs e)
+        {
+            // Se indica la acción que hay que realizar
+            sAccion = "Baja";
+            // Se activan y desactivan los botones correspondientes
+            desactivarAcciones();
+
+        }
+
+        private void btnModificacion_Click(object sender, EventArgs e)
+        {
+            // Se indica la acción que hay que realizar
+            sAccion = "Modificación";
+            // Se activan y desactivan los botones correspondientes
+            desactivarAcciones();
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            // Se activan y desactivan los botones correspondientes
+            activarAcciones();
+
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+        
+
+        // Se termina la acción realizada
+        sAccion = "";
+                    // Se activan y desactivan los botones correspondientes
+                    activarAcciones();
+    } 
+
+  
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            // Se cierra la conexión antes de terminar
+            cerrarConexion();
+            // Se finaliza el programa
+            this.Close();
+
+        }
+    }
+    }
